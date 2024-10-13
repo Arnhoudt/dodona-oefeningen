d03ad3599927c60108ef4f968500defc35b46978
= <hey@antonkindt.be>
2024-10-14 00:28:42 +0200

    sql test

--- exercises/python/tijd-op-mars/solution/solution.py
+++ exercises/python/tijd-op-mars/solution/solution.py
@@ -1,13 +1,15 @@
-cijfer = int(input())
+plan_uren = int(input())
+plan_minuten = int(input())
+
+dagen = int(input())
+uren = int(input())
 minuten = int(input())
 
-delta_minuten = int(input())
+plan_tot_minuten = plan_minuten + plan_uren * 60
+tot_minuten = ((dagen * 24) + uren) * 60 + minuten
 
-graden_cijfer = 360 / 12 * cijfer
-graden_minuten = 360 / 60 * minuten
-graden_delta_minuten = 360 / 60 * delta_minuten
+delta_plan_dagen = tot_minuten // plan_tot_minuten
+delta_plan_uren = (tot_minuten - delta_plan_dagen * plan_tot_minuten) //60
+delta_plan_minuten = tot_minuten - delta_plan_dagen * plan_tot_minuten - delta_plan_uren * 60
 
-verschil = (graden_cijfer - graden_minuten)%360
-
-graden_delta_minuten_basis = graden_delta_minuten - verschil
-print(int(graden_delta_minuten_basis//360+1))
+print("Als er op Aarde ",dagen," dagen, ", uren," uren en ", minuten, " minuten voorbij zijn gegaan, zullen er op deze planeet ", delta_plan_dagen," dagen, ",delta_plan_uren," uren en ",delta_plan_minuten, " minuten verstreken zijn.")
